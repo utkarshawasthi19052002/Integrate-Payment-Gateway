c0f47459531a282b10934de46a3bf015e04200d4
utkarshawasthi19052002 <133538326+utkarshawasthi19052002@users.noreply.github.com>
2023-12-11 09:52:35 +0530

    Delete src directory

--- src/main/java/com/p1/ClientAddRepo.java
+++
@@ -1,9 +0,0 @@
-package com.p1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-public interface ClientAddRepo extends JpaRepository<ClientAdd, Integer>{
-	
-	List<ClientAdd> findByEmail(String email);
-
-}
